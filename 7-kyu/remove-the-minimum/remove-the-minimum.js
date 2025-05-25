f7479c8824c5669590084b36d4a77122d4af26e2
Micheal J. Callaghan <142707770+mjcaldev@users.noreply.github.com>
2025-05-25 01:32:52 -0400

    Add solution - CodeHub

---
+++ 7-kyu/remove-the-minimum/remove-the-minimum.js
@@ -0,0 +1,6 @@
+function removeSmallest(numbers) {
+  if (numbers.length === 0) return [];
+​
+  const minIndex = numbers.indexOf(Math.min(...numbers));
+  return numbers.slice(0, minIndex).concat(numbers.slice(minIndex + 1));
+}
